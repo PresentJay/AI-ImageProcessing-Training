f2823a21007a8cb2b43f34469a1f3d5211596784
PresentJay <presentj94@gmail.com>
2020-12-18 19:54:24 +0900

    parallel & horizontal edge mask

---
+++ [10]convolution/5-horizontal_edge_mask.py
@@ -0,0 +1,29 @@
+import cv2 as cv
+import numpy as np
+from getimg import*
+#smoothing
+roi = gImg('picture5','jpg')
+roi_gray = cv.cvtColor(roi,cv.COLOR_BGR2GRAY)
+
+horizontal_edge_mask = np.array([
+    [1,0,-1],
+    [1,0,-1],
+    [1,0,-1]
+])
+
+output_box = np.zeros((roi.shape[0],roi.shape[1]),dtype=np.uint8)
+
+for j in range(1,roi.shape[0]-1):
+    for i in range(1,roi.shape[1]-1):
+        sum = 0
+        for r in range(-1,2):
+            for c in range(-1,2):
+                sum += roi_gray.item(j+r,i+c)* horizontal_edge_mask.item(r+1,c+1)  #mask 값
+        if np.sum(horizontal_edge_mask)>0:
+            sum //= np.sum(horizontal_edge_mask)
+        output_box.itemset(j,i,sum)
+
+cv.imshow('origin',roi_gray)
+cv.imshow('horizontal_edge_mask',output_box)
+cv.waitKey(0)
+cv.destroyAllWindows()
